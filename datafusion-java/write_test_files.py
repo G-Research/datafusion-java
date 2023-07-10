f81663ae864db10515789dda9d12fdbfb24bc210
Adam Reeve <adreeve@gmail.com>
2023-07-11 09:44:43 +1200

    Support reading compressed Arrow IPC files

--- datafusion-java/write_test_files.py
+++ datafusion-java/write_test_files.py
@@ -1,18 +1,42 @@
+from pathlib import Path
 import pyarrow as pa
 import pyarrow.parquet as pq
 
 
-num_rows = 100
+def main():
+    resource_dir = Path("src/test/resources")
+    write_dictionary_data(resource_dir / "dictionary_data.parquet")
+    write_zstd_ipc_data(resource_dir / "zstd_compressed.arrow")
 
-dict_array_x = pa.DictionaryArray.from_arrays(
-    pa.array([i % 3 for i in range(num_rows)]),
-    pa.array(["one", "two", "three"])
-)
 
-dict_array_y = pa.DictionaryArray.from_arrays(
-    pa.array([i % 3 for i in range(num_rows)]),
-    pa.array(["four", "five", "six"])
-)
+def write_dictionary_data(path):
+    num_rows = 100
 
-table = pa.Table.from_arrays([dict_array_x, dict_array_y], ["x", "y"])
-pq.write_table(table, "src/test/resources/dictionary_data.parquet")
+    dict_array_x = pa.DictionaryArray.from_arrays(
+        pa.array([i % 3 for i in range(num_rows)]),
+        pa.array(["one", "two", "three"])
+    )
+
+    dict_array_y = pa.DictionaryArray.from_arrays(
+        pa.array([i % 3 for i in range(num_rows)]),
+        pa.array(["four", "five", "six"])
+    )
+
+    table = pa.Table.from_arrays([dict_array_x, dict_array_y], ["x", "y"])
+    pq.write_table(table, path)
+
+
+def write_zstd_ipc_data(path):
+    schema = pa.schema([pa.field("x", pa.int64())])
+
+    options = pa.ipc.IpcWriteOptions(compression='zstd')
+    with pa.OSFile(str(path), 'wb') as sink:
+        with pa.ipc.new_file(sink, schema, options=options) as writer:
+            for _ in range(2):
+                xs = pa.array([i % 10 for i in range(500)], type=pa.int64())
+                batch = pa.record_batch([xs], schema)
+                writer.write(batch)
+
+
+if __name__ == '__main__':
+    main()
